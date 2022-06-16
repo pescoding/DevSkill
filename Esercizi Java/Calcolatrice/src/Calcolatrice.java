79c754fd13fc405e277f3a9836be1dc529b27b37
Michele Imparato <micheleimparato96@gmail.com>
2022-06-16 16:23:11 +0200

    Aggiunta la possibilità di inserire i numeri su cui operare da input

--- Esercizi Java/Calcolatrice/src/Calcolatrice.java
+++ Esercizi Java/Calcolatrice/src/Calcolatrice.java
@@ -1,25 +1,29 @@
+import java.util.Scanner;
+
 public class Calcolatrice {
 
     public static void main(String[] args){
 
         System.out.println();
 
+        Scanner scelta1 = new Scanner(System.in);
+        Scanner scelta2 = new Scanner(System.in);
+
+        System.out.print("Inserisci il primo numero su cui operare: ");
+        Integer opt1 = scelta1.nextInt();
+        System.out.println();
+        System.out.print("Inserisci il secondo numero: ");
+        Integer opt2 = scelta2.nextInt();
+        System.out.println();
+
         Numeri numeri = new Numeri();
 
-        numeri.setNumeri(8, 4);
+        numeri.setNumeri(opt1, opt2);
 
-        System.out.println(numeri.addizione());
-        System.out.println(numeri.sottrazione());
-        System.out.println(numeri.moltiplicazione());
-        System.out.println(numeri.divisione());
-
-        numeri.setNumero1(12);
-
-        System.out.println();
-        System.out.println(numeri.addizione());
-        System.out.println(numeri.sottrazione());
-        System.out.println(numeri.moltiplicazione());
-        System.out.println(numeri.divisione());
+        System.out.println(opt1 + " + " + opt2 + " = " + numeri.addizione());
+        System.out.println(opt1 + " - " + opt2 + " = " + numeri.sottrazione());
+        System.out.println(opt1 + " * " + opt2 + " = " + numeri.moltiplicazione());
+        System.out.println(opt1 + " / " + opt2 + " = " + numeri.divisione());
 
     }
 
